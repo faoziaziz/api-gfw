3405d59d37f05843ae74fdeaac769d93ce1deb0b
Aziz Faozi <admin@labseni.com>
2020-09-09 17:11:00 +0700

    add some dependency

--- src/config.h
+++ src/config.h
@@ -2,25 +2,25 @@
 /* src/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Name of package */
-#define PACKAGE "project-name"
+#define PACKAGE "api-gfw-client"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "project-name"
+#define PACKAGE_NAME "api-gfw-client"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "project-name 0.0.1"
+#define PACKAGE_STRING "api-gfw-client 0.0.2"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "project-name"
+#define PACKAGE_TARNAME "api-gfw-client"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0.1"
+#define PACKAGE_VERSION "0.0.2"
 
 /* Version number of package */
-#define VERSION "0.0.1"
+#define VERSION "0.0.2"
