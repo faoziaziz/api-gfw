55ef98386916905254bb42ee21b342638f0eef77
Aziz Faozi <admin@labseni.com>
2020-10-31 13:31:43 +0000

    dada

--- src/main.c
+++ src/main.c
@@ -10,19 +10,19 @@
 
 int main(int argc, char **argv)
 {
-    int check;
-    content_data_gps cont_gps;
-  /* tester checker */
-    // client_tester();
-  client_push(cont_gps);
-  /* for localdatabase */
-  //accgps();
-  //sqlite3_tester();
-  //insert_tester();
-  //test_parameterized();
-  //tester_json2();
-  //db_init();
-  //insert_gps_to_localdb(12.31, 1231.21);
-  
-  return 0;
+	int check;
+	content_data_gps cont_gps;
+	/* tester checker */
+	// client_tester();
+	client_push(cont_gps);
+	/* for localdatabase */
+	//accgps();
+	//sqlite3_tester();
+	//insert_tester();
+	//test_parameterized();
+	//tester_json2();
+	//db_init();
+	//insert_gps_to_localdb(12.31, 1231.21);
+
+	return 0;
 }
